36921d576cb856eab94a3ea1b66d93ebae731da8
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-21 13:32:12 -0600

    I finished M05 - 06

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -41,6 +41,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento);
+   return array;
 }
 
 function dePalabrasAFrase(palabras) {
