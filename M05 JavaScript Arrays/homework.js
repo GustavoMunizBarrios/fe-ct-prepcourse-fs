4f6eb48f6a440ec33039b1f488db8dd5f3381617
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-21 14:00:07 -0600

    I finished M05 - 11

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -77,6 +77,9 @@ function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   let suma = resultadosTest.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+   return suma / resultadosTest.length;
+
 }
 
 function numeroMasGrande(arrayOfNums) {
