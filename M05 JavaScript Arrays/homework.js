ef420ae69708ad7127088435ff988b80f46dddbf
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-22 19:43:51 -0600

    I finished M05 - 22

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -163,6 +163,21 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   let mesesBuscados = ["Enero", "Marzo", "Noviembre"];
+   let mesesEncontrados = [];
+ 
+   for (let i = 0; i < array.length; i++) {
+     if (mesesBuscados.includes(array[i])) {
+       mesesEncontrados.push(array[i]);
+     }
+   }
+ 
+   if (mesesEncontrados.length === 3) {
+     return mesesEncontrados;
+   } else {
+     return "No se encontraron los meses pedidos";
+   }
+
 }
 
 function tablaDelSeis() {
