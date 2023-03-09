34ebdd2841908dd199805cbfbecc16725888b580
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-03-08 20:25:53 -0600

    I finished M07 - 08

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -28,6 +28,9 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+
+   var sujeto = new Persona(nombre, edad);
+   return sujeto; 
 }
 
 function agregarMetodo() {
@@ -35,6 +38,9 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos = function() {
+      return this.nombre + ", " + this.edad + " años"
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
